824c661927f6e9680d33c6662611fda7f9c471d7
Mehul Parmar <mparmar.mailbox@gmail.com>
2023-11-16 08:16:32 +0530

    Added SPM Support
    - Update name of library

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Lightbox-iOS",
+    name: "Lightbox",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Lightbox-iOS",
-            targets: ["Lightbox-iOS"]
+            name: "Lightbox",
+            targets: ["Lightbox"]
         )
     ],
     dependencies: [
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Lightbox-iOS",
+            name: "Lightbox",
             dependencies: [
 				"Imaginary"
 			])
